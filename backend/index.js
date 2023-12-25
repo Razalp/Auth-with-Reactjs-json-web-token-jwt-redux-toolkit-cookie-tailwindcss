313fa9d16111381c726b2b2b6a48333ac2473731
Razalp <razalp0012300@gmail.com>
2023-12-25 12:44:18 +0530

    connecteed database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
-const app=express()
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+const app=express()
+mongoose.connect('mongodb+srv://razalp0012300:CsHcj8rrlJ28wToL@cluster0.pikhe2s.mongodb.net/').then(()=>{
+    console.log('mongoconnected')
+}).catch(()=>{
+    console.log('not connected')
+})
 app.get('/', (req, res) => {
     res.send('hello');
 });
