c7fcc9a80643a12b03def401db7e18cca7259492
Razalp <razalp0012300@gmail.com>
2023-12-26 11:11:53 +0530

    add redux presist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
-import  useReducer from './userSlice'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import useReducer from './userSlice';  
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export const store =configureStore({
-    reducer:{user:useReducer},
-    middleware:(getDefaltMiddleware)=>getDefaltMiddleware({
-        serializableCheck:false,
+const rootReducer = combineReducers({
+  user: useReducer,
+});
 
-    }),
-})
+const persistConfig = {
+  key: 'root',
+  version: 1,  
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);  // Corrected the function name
+
+export const store = configureStore({
+  reducer:  persistedReducer ,
+  middleware: getDefaultMiddleware => getDefaultMiddleware({
+    serializableCheck: false,
+  }),
+});
+
+export const persistor = persistStore(store);
