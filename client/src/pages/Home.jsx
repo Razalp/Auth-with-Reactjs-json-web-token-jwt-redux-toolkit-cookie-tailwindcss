38f69f314d09e1e201564d0e99de80cd6bd31eae
Razalp <razalp0012300@gmail.com>
2023-12-30 09:19:35 +0530

    some changes make

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,10 +7,7 @@ export default function Home() {
         Welcome to my Auth App!
       </h1>
       <p className='mb-4 text-slate-700'>
-        This is a full-stack web application built with the MERN (MongoDB,
-        Express, React, Node.js) stack. It includes authentication features that
-        allow users to sign up, log in, and log out, and provides access to
-        protected routes only for authenticated users.
+        This is a full-stack web application built with the MERN 
       </p>
       <p className='mb-4 text-slate-700'>
         The front-end of the application is built with React and uses React
